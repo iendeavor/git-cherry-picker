8e4730f0307c6bd1ab4adf51662d6eb430f9d865
Ernest Chang <iattempt.net@gmail.com>
2020-08-09 23:57:38 +0800

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/git-cherry-picker/" : "/",
   configureWebpack: {
     plugins: [new Dotenv()]
   },
