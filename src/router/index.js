ded7e7453a2c7fc54285f011802cd83fffa9b6e5
Ernest Chang <iattempt.net@gmail.com>
2020-10-31 21:28:29 +0800

    feat: hide blank page

--- src/router/index.js
+++ src/router/index.js
@@ -38,4 +38,12 @@ const router = new VueRouter({
   routes,
 });
 
+router.beforeEach((to, from, next) => {
+  if (to.path === "/") {
+    next("/cherry-pick");
+  } else {
+    next();
+  }
+});
+
 export default router;
